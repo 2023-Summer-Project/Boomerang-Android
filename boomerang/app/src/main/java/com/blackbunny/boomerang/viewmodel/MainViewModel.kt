f0aac45c044c74334a9624ab2e3b298eaf63bb60
Doyoon Kim <doyoon3312@kakao.com>
2023-08-28 17:45:35 +0900

    REFACTOR::0828 Revise SessionUser fetching process.
      * SessionUser fetching would now use newly modified data class User.
      * Received core user information (uid, userName) would be stored in a PreferenceDataStore.

--- boomerang/app/src/main/java/com/blackbunny/boomerang/viewmodel/MainViewModel.kt
+++ boomerang/app/src/main/java/com/blackbunny/boomerang/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.blackbunny.boomerang.viewmodel
 
+import android.content.Context
+import android.util.Log
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
@@ -7,14 +9,20 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.blackbunny.boomerang.data.MainAppStatus
 import com.blackbunny.boomerang.data.authentication.User
 import com.blackbunny.boomerang.data.authentication.UserRepository
+import com.blackbunny.boomerang.presentation.dataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -23,10 +31,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
 
-    private val _appScreenState = MutableStateFlow(MainAppStatus.INITIAL)
+//    init {
+//        getCurrentUser()
+//    }
+
+    private val _appScreenState = MutableStateFlow(MainAppStatus.STARTUP)
     val appScreenState = _appScreenState.asStateFlow()
 
     fun updateAppScreenState(sessionUser: User?) {
@@ -34,59 +47,77 @@ class MainViewModel @Inject constructor(
         _appScreenState.update {
             if (sessionUser != null) {
                 if (sessionUser.isEmailVerified) {
+                    // Update PreferenceDataStore.
+                    viewModelScope.launch {
+                        updateDatastore(sessionUser)
+                    }
+                    // Update MainAppStatus.
                     MainAppStatus.AUTHENTICATED
                 } else {
                     MainAppStatus.NOT_VERIFIED
                 }
             } else {
-                MainAppStatus.INITIAL
-            }
-        }
-
-
-        if (sessionUser != null) {
-
-            if (sessionUser.isEmailVerified)  {
-                _appScreenState.update {
-                    MainAppStatus.AUTHENTICATED
-                }
-            } else {
-                MainAppStatus.NOT_VERIFIED
-            }
-
-            _appScreenState.update {
-                MainAppStatus.AUTHENTICATED
-            }
-        } else {
-            _appScreenState.update {
-                MainAppStatus.INITIAL
+                MainAppStatus.STARTUP
             }
         }
     }
 
     // Session User
-    private val _sessionUser = MutableStateFlow<User?>(getCurrentUser())
+    private val _sessionUser = MutableStateFlow<User?>(null)
     val sessionUser = _sessionUser.asStateFlow()
 
     // Private function that fetches current user for initialization.
-    private fun getCurrentUser(): User? {
-        return userRepository.fetchCurrentUser().also {
-            updateAppScreenState(it)
+//    suspend fun getCurrentUser() {
+////        return userRepository.fetchCurrentUser().also {
+////            updateAppScreenState(it)
+////        }
+//        viewModelScope.async {
+//            userRepository.fetchSessionUser()
+//                .fold(
+//                    onSuccess = {
+//                        updateAppScreenState(it)
+//                    },
+//                    onFailure = {
+//                        Log.d("MainViewModel", "Unable to fetch the session user information.")
+//                        it.printStackTrace()
+//                    }
+//                )
+//        }
+//    }
+    suspend fun getCurrentUser() = viewModelScope.async {
+        userRepository.fetchSessionUser()
+            .fold(
+                onSuccess = {
+                    updateAppScreenState(it)
+                },
+                onFailure = {
+                    Log.d("MainViewModel", "Unable to fetch the session user information.")
+                    it.printStackTrace()
+                }
+            )
+    }
+
+    private suspend fun updateDatastore(sessionUser: User) {
+        // edit datastore.
+        Log.d("MainViewModel", "Writing current sessionUser infor: $sessionUser")
+        context.dataStore.edit { user_info ->
+            user_info[stringPreferencesKey("username")] = sessionUser.userName
+            user_info[stringPreferencesKey("profile_image")] = sessionUser.profileImage
         }
     }
 
     // Should be called on one of the Activity's lifecycle.
-    fun refreshCurrentUser() {
-        _sessionUser.update {
-            getCurrentUser()
-        }
-    }
+//    fun refreshCurrentUser() {
+//        getCurrentUser()
+//    }
 }
 
 class MainAppState(
     val scaffoldState: ScaffoldState,
     val snackbarScope: CoroutineScope,
-    val navController: NavHostController
+    val navController: NavHostController,
+    val topAppBarVisibility: Boolean,
+    val bottomAppBarVisibility: Boolean
 ) {
     // function for displaying Snackbar. Can be used anywhere (planned).
     fun showSnackbar(message: String, duration: SnackbarDuration) {
@@ -113,6 +144,8 @@ fun rememberMainAppState(
     MainAppState(
         scaffoldState = scaffoldState,
         navController = navController,
-        snackbarScope = snackbarScope
+        snackbarScope = snackbarScope,
+        topAppBarVisibility = false,
+        bottomAppBarVisibility = false
     )
 }
