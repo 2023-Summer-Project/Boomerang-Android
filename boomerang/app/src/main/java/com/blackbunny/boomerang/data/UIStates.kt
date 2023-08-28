06bdfec15039816a9b2e6ffbe27ca6d34930883d
Doyoon Kim <doyoon3312@kakao.com>
2023-08-28 18:02:06 +0900

    FEAT::0828 Add Necessary UI State types.
      * Add necessary data classes for storing UI State on corresponding composable screen.

--- boomerang/app/src/main/java/com/blackbunny/boomerang/data/UIStates.kt
+++ boomerang/app/src/main/java/com/blackbunny/boomerang/data/UIStates.kt
@@ -1,10 +1,41 @@
 package com.blackbunny.boomerang.data
 
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarDuration
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.navigation.NavHostController
 import com.blackbunny.boomerang.data.authentication.User
+import com.blackbunny.boomerang.domain.product.Product
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+data class MainAppState(
+    val scaffoldState: ScaffoldState,
+    val snackbarScope: CoroutineScope,
+    val navController: NavHostController,
+    val topAppBarVisibility: Boolean,
+    val bottomAppBarVisibility: Boolean
+) {
+    fun showSnackbar(message: String, duration: SnackbarDuration) {
+        snackbarScope.launch {
+            scaffoldState.snackbarHostState.showSnackbar(
+                message = message,
+                duration = duration
+            )
+        }
+    }
+}
 
 data class MainUiState(
     val currentUser: User? = null
+)
+
+data class SignInUiState(
+    val currentUser: User? = null,
+    val inputEmail: TextFieldValue = TextFieldValue(),
+    val inputPassword: TextFieldValue = TextFieldValue(),
+    val isProvidedEmailValid: EmailValidation = EmailValidation.INITIAL,
+    val dialogVisibility: Boolean = false
 )
 
 data class SignUpUiState(
@@ -22,4 +53,28 @@ data class SignUpUiState(
     // SignUp Status
     val signUpStatus: Boolean = false,
     val dialogVisibility: Boolean = false
+)
+
+data class MainServiceUiState(
+    val sessionUser: User = User(),
+    val topAppBarVisibility: Boolean = true,
+    val bottomNavigationBarVisibility: Boolean = true,
+    val lazyGridDataSource: List<Product> = emptyList(),
+    val initialDataFetching: DataFetchRequest = DataFetchRequest.READY,
+    val refreshEnabled: Boolean = true,
+    val isRefreshing: Boolean = false,
+    val isDetailViewVisible: Boolean = false,
+    val currentProductOnDetailView: Product? = null,
+    val requestedChatroomId: String? = null,        // Subject to be removed.
+
+    val requestedChatroom: String = "",
+    val temporaryChatroomVisibility: Boolean = false,
+//    val chatroomRequest: Pair<String, Boolean> = Pair("", false)
+)
+
+data class ProfileServiceUiState(
+    val sessionUser: User = User(),
+    val dialogVisibility: Boolean = false,
+    val signOutRequestStatus: SignOutRequest = SignOutRequest.YET_REQUESTED,
+    val isOssNoticeVisible: Boolean = false
 )
